6eec607c90f0512866fca90a2b946f8c738f35ca
Massimo Luraschi <massimo.lura@gmail.com>
2022-04-29 09:31:04 +0200

    Integrated evm-typegen to the template

---
+++ src/contract.ts
@@ -0,0 +1,32 @@
+import { assertNotNull, Store } from "@subsquid/substrate-evm-processor";
+import { ethers } from "ethers";
+import * as erc721 from "./abi/erc721";
+import { Contract } from "./model";
+
+export const contract = new ethers.Contract(
+  "0xb654611f84a8dc429ba3cb4fda9fad236c505a1a",
+  erc721.abi,
+  new ethers.providers.WebSocketProvider(assertNotNull(process.env.CHAIN_NODE))
+);
+
+export function createContractEntity(): Contract {
+  return new Contract({
+    id: contract.address,
+    name: "Moonsama",
+    symbol: "MSAMA",
+    totalSupply: 1000n,
+  });
+}
+
+let contractEntity: Contract | undefined;
+
+export async function getContractEntity({
+  store,
+}: {
+  store: Store;
+}): Promise<Contract> {
+  if (contractEntity == null) {
+    contractEntity = await store.get(Contract, contract.address);
+  }
+  return assertNotNull(contractEntity);
+}
--- src/processor.ts
+++ src/processor.ts
@@ -1,15 +1,18 @@
-import { SubstrateEvmProcessor } from "@subsquid/substrate-evm-processor";
+import {
+  EvmLogHandlerContext,
+  SubstrateEvmProcessor,
+} from "@subsquid/substrate-evm-processor";
 import { lookupArchive } from "@subsquid/archive-registry";
-import { CHAIN_NODE, BATCH_SIZE, CONTRACT_ADDRESS } from "./constants";
-import { contractLogsHandler, createContractEntity } from "./helpers/events";
-import { events } from "./abis/erc721";
+import { contract, createContractEntity, getContractEntity } from "./contract";
+import * as erc721 from "./abi/erc721";
+import { Owner, Token, Transfer } from "./model";
 
 const processor = new SubstrateEvmProcessor("moonriver-substrate");
 
-processor.setBatchSize(BATCH_SIZE);
+processor.setBatchSize(500);
 
 processor.setDataSource({
-  chain: CHAIN_NODE,
+  chain: "wss://wss.api.moonriver.moonbeam.network",
   archive: lookupArchive("moonriver")[0].url,
 });
 
@@ -20,11 +23,56 @@ processor.addPreHook({ range: { from: 0, to: 0 } }, async (ctx) => {
 });
 
 processor.addEvmLogHandler(
-  CONTRACT_ADDRESS,
+  contract.address,
   {
-    filter: [events["Transfer(address,address,uint256)"].topic],
+    filter: [erc721.events["Transfer(address,address,uint256)"].topic],
   },
   contractLogsHandler
 );
+
+export async function contractLogsHandler(
+  ctx: EvmLogHandlerContext
+): Promise<void> {
+  const transfer =
+    erc721.events["Transfer(address,address,uint256)"].decode(ctx);
+
+  let from = await ctx.store.get(Owner, transfer.from);
+  if (from == null) {
+    from = new Owner({ id: transfer.from, balance: 0n });
+    await ctx.store.save(from);
+  }
+
+  let to = await ctx.store.get(Owner, transfer.to);
+  if (to == null) {
+    to = new Owner({ id: transfer.to, balance: 0n });
+    await ctx.store.save(to);
+  }
+
+  let token = await ctx.store.get(Token, transfer.tokenId.toString());
+  if (token == null) {
+    token = new Token({
+      id: transfer.tokenId.toString(),
+      uri: await contract.tokenURI(transfer.tokenId),
+      contract: await getContractEntity(ctx),
+      owner: to,
+    });
+    await ctx.store.save(token);
+  } else {
+    token.owner = to;
+    await ctx.store.save(token);
+  }
+
+  await ctx.store.save(
+    new Transfer({
+      id: ctx.txHash,
+      token,
+      from,
+      to,
+      timestamp: BigInt(ctx.substrate.block.timestamp),
+      block: ctx.substrate.block.height,
+      transactionHash: ctx.txHash,
+    })
+  );
+}
 
 processor.run();

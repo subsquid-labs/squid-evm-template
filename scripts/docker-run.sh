9d63e9d87f3a21927ee984c35374680881b18920
dzhelezov <dzhelezov@gmail.com>
2022-10-21 16:33:56 +0200

    minimal template

--- scripts/docker-run.sh
+++
@@ -1,5 +0,0 @@
-set -e
-docker build . --target processor -t squid-processor
-# make sure the port matches .env. 
-# For Linux, add --add-host=host.docker.internal:host-gateway
-docker run --rm -e DB_HOST=host.docker.internal --env-file=.env squid-processor
--- src/processor.ts
+++ src/processor.ts
@@ -1,154 +1,23 @@
-import { lookupArchive } from "@subsquid/archive-registry";
 import { Store, TypeormDatabase } from "@subsquid/typeorm-store";
-import {
-  BatchContext,
-  BatchProcessorItem,
-  EvmLogEvent,
-  SubstrateBatchProcessor,
-  SubstrateBlock,
-} from "@subsquid/substrate-processor";
-import { In } from "typeorm";
-import { ethers } from "ethers";
-import { CHAIN_NODE, contractAddress, getContractEntity } from "./contract";
-import { Owner, Token, Transfer } from "./model";
-import * as erc721 from "./abi/erc721";
+import {EvmBatchProcessor} from '@subsquid/evm-processor'
 
-const database = new TypeormDatabase();
-const processor = new SubstrateBatchProcessor()
-  .setBatchSize(500)
+const processor = new EvmBatchProcessor()
   .setDataSource({
-    chain: CHAIN_NODE,
-    archive: lookupArchive("moonriver", { release: "FireSquid" }),
+    chain: process.env.ETHEREUM_MAINNET_RPC,
+    archive: 'https://eth-test.archive.subsquid.io',
   })
-  .setTypesBundle("moonbeam")
-  .addEvmLog(contractAddress, {
-    filter: [erc721.events["Transfer(address,address,uint256)"].topic],
-  });
-
-type Item = BatchProcessorItem<typeof processor>;
-type Context = BatchContext<Store, Item>;
-
-processor.run(database, async (ctx) => {
-  const transfersData: TransferData[] = [];
-
-  for (const block of ctx.blocks) {
-    for (const item of block.items) {
-      if (item.name === "EVM.Log") {
-        const transfer = handleTransfer(ctx, block.header, item.event);
-        transfersData.push(transfer);
-      }
-    }
-  }
-
-  await saveTransfers(ctx, transfersData);
+  .addLog('0x....', {
+    filter: [[ ]],
+    data: {
+        evmLog: {
+            topics: true,
+            data: true,
+        },
+    } as const,
 });
 
-type TransferData = {
-  id: string;
-  from: string;
-  to: string;
-  token: ethers.BigNumber;
-  timestamp: bigint;
-  block: number;
-  transactionHash: string;
-};
 
-function handleTransfer(
-  ctx: Context,
-  block: SubstrateBlock,
-  event: EvmLogEvent
-): TransferData {
-  const { from, to, tokenId } = erc721.events[
-    "Transfer(address,address,uint256)"
-  ].decode(event.args);
+processor.run(new TypeormDatabase(), async (ctx) => {
+    ctx.log.info(ctx.blocks, "Got blocks")
+});
 
-  const transfer: TransferData = {
-    id: event.id,
-    token: tokenId,
-    from,
-    to,
-    timestamp: BigInt(block.timestamp),
-    block: block.height,
-    transactionHash: event.evmTxHash,
-  };
-
-  return transfer;
-}
-
-async function saveTransfers(ctx: Context, transfersData: TransferData[]) {
-  const tokensIds: Set<string> = new Set();
-  const ownersIds: Set<string> = new Set();
-
-  for (const transferData of transfersData) {
-    tokensIds.add(transferData.token.toString());
-    ownersIds.add(transferData.from);
-    ownersIds.add(transferData.to);
-  }
-
-  const transfers: Set<Transfer> = new Set();
-
-  const tokens: Map<string, Token> = new Map(
-    (await ctx.store.findBy(Token, { id: In([...tokensIds]) })).map((token) => [
-      token.id,
-      token,
-    ])
-  );
-
-  const owners: Map<string, Owner> = new Map(
-    (await ctx.store.findBy(Owner, { id: In([...ownersIds]) })).map((owner) => [
-      owner.id,
-      owner,
-    ])
-  );
-
-  for (const transferData of transfersData) {
-    const contract = new erc721.Contract(
-      ctx,
-      { height: transferData.block },
-      contractAddress
-    );
-
-    let from = owners.get(transferData.from);
-    if (from == null) {
-      from = new Owner({ id: transferData.from, balance: 0n });
-      owners.set(from.id, from);
-    }
-
-    let to = owners.get(transferData.to);
-    if (to == null) {
-      to = new Owner({ id: transferData.to, balance: 0n });
-      owners.set(to.id, to);
-    }
-
-    const tokenId = transferData.token.toString();
-
-    let token = tokens.get(tokenId);
-    if (token == null) {
-      token = new Token({
-        id: tokenId,
-        uri: await contract.tokenURI(transferData.token),
-        contract: await getContractEntity(ctx.store),
-      });
-      tokens.set(token.id, token);
-    }
-    token.owner = to;
-
-    const { id, block, transactionHash, timestamp } = transferData;
-
-    const transfer = new Transfer({
-      id,
-      block,
-      timestamp,
-      transactionHash,
-      from,
-      to,
-      token,
-    });
-
-    transfers.add(transfer);
-  }
-
-  await ctx.store.save([...owners.values()]);
-  await ctx.store.save([...tokens.values()]);
-  await ctx.store.save([...transfers]);
-}

723db8c5c3ca5bb068cac82f7f0076121ded4c8a
dzhelezov <dzhelezov@gmail.com>
2022-11-07 12:23:41 +0100

    index transactions to the black hole

--- src/processor.ts
+++ src/processor.ts
@@ -1,4 +1,4 @@
-import { Store, TypeormDatabase } from "@subsquid/typeorm-store";
+import { TypeormDatabase } from "@subsquid/typeorm-store";
 import {EvmBatchProcessor} from '@subsquid/evm-processor'
 
 const processor = new EvmBatchProcessor()
@@ -6,18 +6,29 @@ const processor = new EvmBatchProcessor()
     chain: process.env.ETHEREUM_MAINNET_WSS,
     archive: 'https://eth.archive.subsquid.io',
   })
-  .addLog('0x0000000000000000000000000000000000000000', {
-    filter: [[ ]],
+  .addTransaction([
+    '0x0000000000000000000000000000000000000000'
+  ], {
+    range: {
+      from: 10_000_000
+    },
     data: {
-        evmLog: {
-            topics: true,
-            data: true,
-        },
-    } as const,
-});
+      transaction: {
+        from: true,
+        input: true,
+        to: true
+      }
+    }
+  });
 
 
 processor.run(new TypeormDatabase(), async (ctx) => {
-    ctx.log.info(ctx.blocks, "Got blocks")
+  for (let c of ctx.blocks) {
+    for (let i of c.items) {
+      // apply arbitrary data transformation logic here
+      // use ctx.store to persist the data
+      ctx.log.info(i, "Next item:")
+    }
+  }
 });
 

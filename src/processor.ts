e3b7fa924598910e1f0b5929f1be6d6a329a10b9
dzhelezov <dzhelezov@gmail.com>
2022-12-02 11:43:41 +0100

    update libs

--- src/processor.ts
+++ src/processor.ts
@@ -6,18 +6,32 @@ import { Gravatar } from "./model/generated/gravatar.model";
 
 const processor = new EvmBatchProcessor()
   .setDataSource({
-    // uncomment and set ETHEREUM_MAINNET_WSS to 
-    // a websocket JSON-RPC Etheruem mainnet endpoint in
-    // order to enable contract state queries:
-    // chain: process.env.ETHEREUM_MAINNET_WSS,
+    // uncomment and set RPC_ENDPOONT to enable contract state queries. 
+    // Both https and wss endpoints are supported. 
+    // chain: process.env.RPC_ENDPOINT,
+
+    // Change the Archive endpoints for run the squid 
+    // against the other  EVM networks:
+    // Polygon: https://polygon.archive.subsquid.io
+    // Goerli: https://goerli.archive.subsquid.io
+
     archive: 'https://eth.archive.subsquid.io',
   })
+<<<<<<< HEAD
   .setBlockRange({ from: 6175243 })
   .addLog('0x2E645469f354BB4F5c8a05B3b30A929361cf77eC', {
     filter: [[
       events['NewGravatar(uint256,address,string,string)'].topic,
       events['UpdatedGravatar(uint256,address,string,string)'].topic,
    ]],
+=======
+  .addTransaction([
+    '0x0000000000000000000000000000000000000000'
+  ], {
+    range: {
+      from: 6_000_000
+    },
+>>>>>>> c13fa44 (update libs)
     data: {
         evmLog: {
             topics: true,

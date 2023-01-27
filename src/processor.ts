ff452b9db6c4c1a26196b99b61d858c63a6648e9
dzhelezov <dzhelezov@gmail.com>
2023-01-27 19:40:07 +0100

    more meaninful template saving blackhole burns

--- src/processor.ts
+++ src/processor.ts
@@ -1,6 +1,8 @@
 import { TypeormDatabase } from '@subsquid/typeorm-store';
 import {EvmBatchProcessor} from '@subsquid/evm-processor'
 import { lookupArchive } from '@subsquid/archive-registry'
+import assert from 'assert';
+import { Burn } from './model';
 
 const processor = new EvmBatchProcessor()
   .setDataSource({
@@ -24,20 +26,39 @@ const processor = new EvmBatchProcessor()
     data: {
       transaction: {
         from: true,
-        input: true,
-        to: true
+        value: true,
+        hash: true
       }
     }
   });
 
+function formatID(height:any, hash:string) {
+  return `${String(height).padStart(10, '0')}-${hash.slice(3,8)}` 
+
+} 
 
 processor.run(new TypeormDatabase(), async (ctx) => {
+  const burns: Burn[] = []
   for (let c of ctx.blocks) {
     for (let i of c.items) {
-      // apply arbitrary data transformation logic here
-      // use ctx.store to persist the data
-      ctx.log.info(i, "Next item:")
+      assert(i.kind == 'transaction')
+      // decode and normalize the tx data
+      burns.push(new Burn({
+        id: formatID(c.header.height, i.transaction.hash),
+        block: c.header.height,
+        address: i.transaction.from,
+        value: i.transaction.value,
+        txHash: i.transaction.hash
+      }))
     }
-  }
+   }
+   // apply vectorized transformations and aggregations
+   const burned = burns.reduce((acc, b) => acc + b.value, 0n)/1_000_000_000n
+   const startBlock = ctx.blocks.at(0)?.header.height
+   const endBlock = ctx.blocks.at(-1)?.header.height
+   ctx.log.info(`Burned ${burned} Gwei from ${startBlock} to ${endBlock}`)
+
+   // upsert batches of entities with batch-optimized ctx.store.save
+   await ctx.store.save(burns)
 });
 

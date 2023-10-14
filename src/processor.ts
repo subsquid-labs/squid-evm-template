0350adcb5ef3ef5881476bdbcbefe9fee0418ada
abernatskiy <abernatskiy@toha.li>
2023-10-14 17:09:30 +0900

    Rate-limiting the RPC by default
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -18,7 +18,10 @@ export const processor = new EvmBatchProcessor()
 
         // Must be set for RPC ingestion (https://docs.subsquid.io/evm-indexing/evm-processor/)
         // OR to enable contract state queries (https://docs.subsquid.io/evm-indexing/query-state/)
-        chain: 'https://rpc.ankr.com/eth',
+        chain: {
+            url: 'https://rpc.ankr.com/eth',
+            rateLimit: 10
+        }
     })
     .setFinalityConfirmation(75)
     .setFields({

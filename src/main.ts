a1e698a9d36a758d059a695a7ec4718563ec2cf9
belopash <abelopashentsev@gmail.com>
2024-01-18 18:38:14 +0500

    transfer mock db to separate package

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,5 @@
-import {
-    HotDatabaseState,
-    HotDatabase,
-    FinalTxInfo,
-    HotTxInfo,
-    HashAndHeight,
-} from '@subsquid/util-internal-processor-tools'
 import {processor} from './processor'
-
-class MockDatabase implements HotDatabase<unknown> {
-    readonly supportsHotBlocks = true
-
-    async connect(): Promise<HotDatabaseState> {
-        return {height: -1, hash: '0x', top: []}
-    }
-
-    async transact(info: FinalTxInfo, cb: (store: unknown) => Promise<void>): Promise<void> {
-        return await cb(null)
-    }
-
-    async transactHot(info: HotTxInfo, cb: (store: unknown, block: HashAndHeight) => Promise<void>): Promise<void> {
-        for (let b of info.newBlocks) {
-            await cb(null, b)
-        }
-    }
-}
+import {MockDatabase} from '@belopash/mock-store'
 
 processor.run(new MockDatabase(), async (ctx) => {
     for (let c of ctx.blocks) {

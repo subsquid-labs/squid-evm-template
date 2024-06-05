7b019d71909b3a2a39f6ae5eff833dba78b79947
abernatskiy <abernatskiy@toha.li>
2024-06-06 03:46:58 +0900

    Removing the demo secret in favor of an example of using RPC proxy
    	modified:   .env
    	modified:   squid.yaml
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -18,7 +18,7 @@ export const processor = new EvmBatchProcessor()
     .setRpcEndpoint({
         // Set the URL via .env for local runs or via secrets when deploying to Subsquid Cloud
         // https://docs.subsquid.io/deploy-squid/env-variables/
-        url: assertNotNull(process.env.RPC_ENDPOINT),
+        url: assertNotNull(process.env.RPC_ETH_HTTP, 'No RPC endpoint supplied'),
         // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
         rateLimit: 10
     })
